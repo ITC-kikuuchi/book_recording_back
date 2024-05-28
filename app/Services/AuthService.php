df90dde19b3de7fe216830f3600e452610b782b2
kikuuchi <kikuuchi@itcowork.co.jp>
2024-05-28 14:35:14 +0900

    feat: use 処理の実装

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\User;
+use App\Repositories\User\UserRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
 class AuthService
 {
+    use ExceptionHandlerTrait;
+
     /**
      * AuthService コンストラクタ
      * UserRepositoryInterface の依存性を注入する
