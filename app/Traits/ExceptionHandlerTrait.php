f4db14359b295303cad597117fb2cad216fe2650
kikuuchi <kikuuchi@itcowork.co.jp>
2024-05-28 13:22:59 +0900

    feat: use 処理の実装

--- app/Traits/ExceptionHandlerTrait.php
+++ app/Traits/ExceptionHandlerTrait.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
+use App\Exceptions\NotFoundException;
+use App\Exceptions\UnauthorizedException;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
 trait ExceptionHandlerTrait
 {
+    use ResponseTrait;
+
 }
