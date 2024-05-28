6826d24d8d77aa6697143e013c553f74d58d3802
kikuuchi <kikuuchi@itcowork.co.jp>
2024-05-28 13:32:27 +0900

    feat: コンストラクタメソッドの実装

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -6,4 +6,13 @@ namespace App\Services;
 
 class AuthService
 {
+    /**
+     * AuthService コンストラクタ
+     * UserRepositoryInterface の依存性を注入する
+     *
+     * @param UserRepositoryInterface $userRepositoryInterface
+     */
+    public function __construct(protected UserRepositoryInterface $userRepositoryInterface)
+    {
+    }
 }
