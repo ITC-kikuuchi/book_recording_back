6d3e59635671a48de4e71660390b6a75ae66735b
kikuuchi <kikuuchi@itcowork.co.jp>
2024-05-28 13:23:13 +0900

    feat: 例外発生時の処理の実装

--- app/Traits/ExceptionHandlerTrait.php
+++ app/Traits/ExceptionHandlerTrait.php
@@ -14,4 +14,29 @@ trait ExceptionHandlerTrait
 {
     use ResponseTrait;
 
+    /**
+     * 例外発生時の処理
+     *
+     * @param Exception $e
+     * @return JsonResponse
+     */
+    private function exceptionHandler(Exception $e): JsonResponse
+    {
+        if ($e instanceof UnauthorizedException) {
+            // ログにエラーメッセージを出力
+            Log::error($e);
+            // 401エラー（権限関連の例外）
+            return $this->unauthorizedResponse();
+        } elseif ($e instanceof NotFoundException) {
+            // ログにエラーメッセージを出力
+            Log::error($e);
+            // 404エラー（データ参照失敗の例外）
+            return $this->notFoundResponse();
+        } else {
+            // ログにエラーメッセージを出力
+            Log::error($e);
+            // 500エラー（その他の例外）
+            return $this->internalServerErrorResponse(['errors' => $e->getMessage()]);
+        }
+    }
 }
