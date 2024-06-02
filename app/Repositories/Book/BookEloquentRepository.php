e193dfd30636070963466e26168dd39f11a2dc75
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-02 21:12:26 +0900

    feat: コンストラクタメソッドの実装

--- app/Repositories/Book/BookEloquentRepository.php
+++ app/Repositories/Book/BookEloquentRepository.php
@@ -6,4 +6,13 @@ namespace App\Repositories\Book;
 
 class BookEloquentRepository implements BookRepositoryInterface
 {
+    /**
+     * BookEloquentRepository コンストラクタ
+     * Book の依存性を注入する
+     *
+     * @param Book $book
+     */
+    public function __construct(protected Book $book)
+    {
+    }
 }
