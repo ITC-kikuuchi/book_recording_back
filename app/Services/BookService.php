61a233ca2ee757c754741608d1a36369f3df752a
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-02 21:14:47 +0900

    feat: 書籍一覧取得処理の実装

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -24,4 +24,34 @@ class BookService
     public function __construct(protected BookRepositoryInterface $bookRepositoryInterface)
     {
     }
+
+    /**
+     * 書籍一覧取得
+     *
+     * @return JsonResponse
+     */
+    public function getBooks(): JsonResponse
+    {
+        // 初期値設定
+        $responseData = [];
+        try {
+            // 書籍一覧取得
+            $books = $this->bookRepositoryInterface->getBooks();
+            // レスポンスデータの作成
+            foreach ($books as $book) {
+                $responseData[] = [
+                    Book::ID => $book[Book::ID],
+                    Book::TITLE => $book[Book::TITLE],
+                    Book::AUTHOR => $book[Book::AUTHOR],
+                    Book::GENRE => $book[Book::GENRE],
+                    Book::COVER_IMAGE => $book[Book::COVER_IMAGE] ? asset(PathConst::PUBLIC_BOOK_PATH . '/' . $book[Book::COVER_IMAGE]) : null,
+                ];
+            }
+        } catch (Exception $e) {
+            // エラーハンドリング
+            return $this->exceptionHandler($e);
+        }
+        // 200 レスポンス
+        return $this->okResponse($responseData);
+    }
 }
