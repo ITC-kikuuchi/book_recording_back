39f0622f70fa25e5c1582e08217f153e58c75a72
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-02 21:13:29 +0900

    feat: use 処理の実装

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Consts\PathConst;
+use App\Models\Book;
+use App\Repositories\Book\BookRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class BookService
 {
+    use ExceptionHandlerTrait;
+
 }
