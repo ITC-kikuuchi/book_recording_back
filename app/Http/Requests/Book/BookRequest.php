186c1a25c3e7f9b25990c89af41ca140a0dbe87d
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-04 19:13:45 +0900

    feat: use 処理の実装

--- app/Http/Requests/Book/BookRequest.php
+++ app/Http/Requests/Book/BookRequest.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Requests\Book;
 
+use App\Models\Book;
+use App\Traits\ResponseTrait;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class BookRequest extends FormRequest
 {
+    use ResponseTrait;
+
     /**
      * Determine if the user is authorized to make this request.
      */
