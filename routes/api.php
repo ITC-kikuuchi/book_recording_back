076d1cd55bdcab3916dac180a43193f227ad4ee5
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-11 19:57:37 +0900

    feat: use 処理の実装

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\MemoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
