ff8f98ff3080a7c268e52f009d42e8c37c719952
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-14 15:49:19 +0900

    feat: コンストラクタメソッドの実装

--- app/Repositories/Memo/MemoEloquentRepository.php
+++ app/Repositories/Memo/MemoEloquentRepository.php
@@ -8,4 +8,13 @@ use App\Models\Memo;
 
 class MemoEloquentRepository implements MemoRepositoryInterface
 {
+    /**
+     * MemoEloquentRepository コンストラクタ
+     * Memo の依存性を注入する
+     *
+     * @param Memo $memo
+     */
+    public function __construct(protected Memo $memo)
+    {
+    }
 }
