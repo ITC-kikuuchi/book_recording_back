640c4617a731d197e75590f5862c360a9bd16a99
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-14 16:02:30 +0900

    feat: use 処理の実装

--- app/Services/MemoService.php
+++ app/Services/MemoService.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Memo;
 use App\Repositories\Memo\MemoRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 class MemoService
 {
+    use ExceptionHandlerTrait;
+
     /**
      * MemoService コンストラクタ
      * MemoRepositoryInterface の依存性を注入する
